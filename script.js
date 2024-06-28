c9669d24dfc7d85f3b6464b930d63c6629d610e3
CMYKhaos <cmykhaos19@gmail.com>
2024-06-28 17:44:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function ClearFields() {
+
+    document.getElementById("textfield1").value = "";
+    document.getElementById("textfield2").value = "";
+}
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Erro', 'warning')
+  })
+}
